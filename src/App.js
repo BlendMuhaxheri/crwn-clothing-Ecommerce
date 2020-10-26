d7f9ae5be73e31a0a64f7f893d7ba2751f7fdc4e
unknown <blend11muhaxheri@gmail.com>
2020-10-26 19:11:58 +0100

    broken my homepage into smaller components and fineshed styling my menu-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { HomePage } from "../src/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
+import MenuItem from "./components/menu-item/menu-item.component";
 
 function App() {
   return (
